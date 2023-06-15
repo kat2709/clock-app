a72e0504d9c7d38d120dbac53f2ca7a06399cdab
Kate Rubchenko <6923845@mail.ru>
2023-06-15 18:10:19 +0300

    feat: update audio and add weather widgets

--- index.js
+++ index.js
@@ -32,30 +32,102 @@ prevImgBtn.addEventListener("click", () => {
   document.body.style.background = `url("${imgArr[count]}") center/cover, rgba(0, 0, 0, 0.5)`;
 });
 
-// sounds
-const soundsArr = [
-  { url: "./assets/sounds/Aqua Caelestis.mp3", name: "Aqua Caelestis" },
-  { url: "./assets/sounds/Ennio Morricone.mp3", name: "Ennio Morricone" },
-  { url: "./assets/sounds/River Flows in You.mp3", name: "River Flows in You" },
-  { url: "./assets/sounds/Summer Wind.mp3", name: "Summer Wind" },
+// audio player
+const playList = document.querySelector(".play-list");
+const prevBtn = document.querySelector(".play-prev");
+const nextBtn = document.querySelector(".play-next");
+const playBtn = document.querySelector(".play");
+const audio = document.querySelector("#audio");
+const songTitle = document.querySelector(".song-title");
+const progressContainer = document.querySelector(".progress-container");
+const progress = document.querySelector(".progress");
+
+const songs = [
+  "Aqua Caelestis",
+  "Ennio Morricone",
+  "River Flows in You",
+  "Summer Wind",
 ];
 
-let musicIdx = 0;
+let songIndex = 2;
 
-const prevSoundsBtn = document.querySelector(".play-prev");
-const nextSoundsBtn = document.querySelector(".play-next");
-const playSoundsBtn = document.querySelector(".play");
+loadMusic(songs[songIndex]);
 
-let audio = new Audio(`${soundsArr[musicIdx].url}`);
+function loadMusic(song) {
+  songTitle.innerHTML = song;
+  audio.src = `./assets/sounds/${song}.mp3`;
+}
 
-playSoundsBtn.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.play();
+function playSong() {
+  playList.classList.add("show-song");
+
+  playBtn.classList.remove("play");
+  playBtn.classList.add("pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  playList.classList.remove("show-song");
+
+  playBtn.classList.add("play");
+  playBtn.classList.remove("pause");
+
+  audio.pause();
+}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = playList.classList.contains("show-song");
+
+  if (isPlaying) {
+    pauseSong();
   } else {
-    audio.pause();
+    playSong();
   }
-  playSoundsBtn.classList.toggle("pause");
 });
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadMusic(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadMusic(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+audio.addEventListener("ended", nextSong);
 
 // time
 const hoursDisplay = document.querySelector("#hours");
@@ -131,3 +203,52 @@ let countdown = setInterval(showTime, 1000);
 function changeGreeting() {}
 
 showTime();
+
+// weather
+const weatherBox = document.querySelector(".weather-box");
+const inputCity = document.querySelector(".city");
+const weatherIcon = document.querySelector(".weather-icon");
+const temp = document.querySelector(".temperature");
+const weatherDescr = document.querySelector(".weather-description");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+
+loadWeather();
+
+async function loadWeather() {
+  let value = "Minsk";
+  const urlMinsk = `https://api.openweathermap.org/data/2.5/weather?q=${value}&lang=en&appid=797ef741d117c2472e6a5597e0adaffc&units=metric`;
+  const resMinsk = await fetch(urlMinsk);
+  const dataMinsk = await resMinsk.json();
+  // console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+
+  weatherIcon.classList.add(`owf-${dataMinsk.weather[0].id}`);
+  temp.textContent = `${dataMinsk.main.temp}°C`;
+  weatherDescr.textContent = dataMinsk.weather[0].description;
+  wind.textContent = `Wind speed: ${dataMinsk.wind.speed} m/s`;
+  humidity.textContent = `Humidity: ${dataMinsk.main.humidity}%`;
+
+  weatherBox.classList.remove("err");
+}
+
+async function showWeather() {
+  try {
+    let value = inputCity.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&lang=en&appid=797ef741d117c2472e6a5597e0adaffc&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.dir(data);
+
+    weatherIcon.classList = "weather-icon owf";
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temp.textContent = `${data.main.temp}°C`;
+    weatherDescr.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${data.wind.speed} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    weatherBox.classList.remove("err");
+  } catch (err) {
+    weatherBox.classList.add("err");
+  }
+}
+
+inputCity.addEventListener("keyup", showWeather);
